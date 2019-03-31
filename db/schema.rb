b4b936eae85b6099c16ffdc42ba27e4150812b7e
wilbobraggins <wilbobraggins@gmail.com>
2019-03-31 18:01:17 +0000

    remove

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_234225) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "searches", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.string "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
