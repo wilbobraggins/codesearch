4239a8f76d93bdd457122cc6a305eaff6f679653
wilbobraggins <wilbobraggins@gmail.com>
2019-04-01 21:30:24 +0000

    search controller create and model websearch

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,24 +1,8 @@
 class SearchController < ApplicationController
- attr_accessor :answer
- helper_method :searches
+ 
   
   def index
+    redirect_to Search.websearch(search)
   end
-
-  def websearch(search)
-    url = 'https://stackoverflow.com'
-    if search.scan(/\w+/).size <= 1
-      url + '/questions/tagged/' + "#{search}"
-    else
-      url + '/search?q=' + "#{search}".gsub!(/\s/,'+')
-    end
-  end
-
-  def searches
-    @answer = gets
-    websearch(answer)
-  end
-
-
 
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,11 @@
 class Search < ApplicationRecord
+
+  def websearch(search)
+    url = 'https://stackoverflow.com'
+    if search.scan(/\w+/).size <= 1
+      url + '/questions/tagged/' + "#{search}"
+    else
+      url + '/search?q=' + "#{search}".gsub!(/\s/,'+')
+    end
+  end
 end
