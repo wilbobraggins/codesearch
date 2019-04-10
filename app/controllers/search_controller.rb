8567d46a1c595801b94dd397d2dbdf64e48131aa
wilbobraggins <wilbobraggins@gmail.com>
2019-04-10 18:57:25 +0000

    revised search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,16 +2,10 @@ class SearchController < ApplicationController
  
   
   def index
-
+    
   end
 
   def create
-    @answer = Search.new
-    redirect_to show_path
-  end
-
-  def show
-  Resaults.new.pull_resaults 
   end
 
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,9 +1,4 @@
 class Search < ApplicationRecord
-  attr_accessor :answer
-
-  def initialize
-    @answer = answer
-  end
 
   def websearch(search)
     url = 'https://stackoverflow.com'
@@ -11,43 +6,6 @@ class Search < ApplicationRecord
       url + '/questions/tagged/' + "#{search}"
     else
       url + '/search?q=' + "#{search}".gsub!(/\s/,'+')
-    end
-  end
-
-  def searcher
-    websearch(answer)
-  end
-
-
-class Resault 
-require 'nokogiri'
-require 'httparty'
-require 'byebug'
-
-attr_accessor :page_parsed, :page_unparsed, :list
-
-  def initialize
-    @page_unparsed = page_unparsed
-    @page_parsed = page_parsed
-    @list = list
-  end
-
-
-
-  def pull_resaults
-    url = Search.new.searcher
-    page_unparsed = HTTParty.get(url)
-    page_parsed = Nokogiri::HTML(page_unparsed)
-    list = Array.new
-    listing = page_parsed.css('div.question-summary')
-
-    listing.each do |val|
-      res = {
-        title:        val.css('a.question-hyperlink'),
-        description:  val.css('div.excerpt'),
-        url:          'https://stackoverflow.com' + val.css('a')[0].attributes['href'].value
-      }
-      list << res
     end
   end
 end
