5e7572a12bc7fa8fff82a8dc3d4fe4e6de10d94e
wilbobraggins <wilbobraggins@gmail.com>
2019-04-25 04:02:37 +0000

    controller update

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,27 @@ class SearchController < ApplicationController
  
   
   def index
-    if params[:q].blank?
+    @find = params[:q]
+    if @find.blank?
       flash[:alert] = "Search for something"
     else
-      @find = Search.parse(params)
+      @find = params[:q]
+      url = "https://stackoverflow.com/search?q=" + @find
+      page_unparsed = HTTParty.get(url)
+      page_parsed = Nokogiri::HTML(page_unparsed)
+      list = Array.new
+      listing = page_parsed.css('div.question-summary')
+
+      listing.each do |val|
+          @res = {
+            title:        val.css('a.question-hyperlink'),
+            description:  val.css('div.excerpt'),
+            url:          'https://stackoverflow.com' + val.css('a')[0].attributes['href'].value
+          }
+        list << @res 
+
+      end
+      @res.save
     end
   end
-
 end
